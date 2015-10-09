a24cc57f8672f7c92151856d789dabbfb6971304
Faruh Jumabayev <faruh_jumabayev@mail.ru>
2015-10-09 21:57:43 +0300

    TriangleApp - project

---
+++ TriangleApp/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TriangleApp.Models;
+
+namespace TriangleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Home/FindHypotenuse
+        [HttpGet]
+        public ActionResult FindHypotenuse()
+        {
+            return View();
+        }
+
+        [System.Web.Mvc.HttpPost] // метод будет вызываться только при POST запросах
+        public ActionResult FindHypotenuse(Triangle triangle)
+        {
+            ViewBag.Hypotenuse = Math.Sqrt(Math.Pow(triangle.A, 2) + Math.Pow(triangle.B, 2));
+
+            return View(triangle);
+        }
+
+        [System.Web.Mvc.HttpGet] // метод будет вызываться только при POST запросах
+        public ActionResult FindCathetus()
+        {
+            return View();
+        }
+
+        [HttpPost] // метод будет вызываться только при POST запросах
+        public ActionResult FindCathetus(Triangle triangle)
+        {
+            if (ModelState.IsValid)
+            {
+                double result = Math.Sqrt(Math.Pow(triangle.C, 2) - Math.Pow(triangle.B, 2));
+                if (!Double.IsNaN(result))
+                {
+                    ViewBag.Cathetus = result;
+                }
+                else
+                {
+                    ViewBag.Cathetus = "Значение гипотенузы должно быть больше значения катета";
+                }
+
+            }
+            return View();
+        }
+
+        public ActionResult CalculateArea()
+        {
+            return View();
+        }
+
+        [HttpPost] // метод будет вызываться только при POST запросах
+        public ActionResult CalculateArea(Triangle triangle)
+        {
+            if (ModelState.IsValid)
+            {
+                if (triangle.A + triangle.B < triangle.C ||
+                triangle.A + triangle.C < triangle.B ||
+                triangle.B + triangle.C < triangle.A)
+                {
+                    ViewBag.Msg = @"Данный треугольник является не равным. Попробуйте другие значения.";
+                }
+                else
+                {
+                    ViewBag.Msg = @"Данный треугольник является равным";
+
+                    double p = (triangle.A + triangle.B + triangle.C) / 2.0;
+                    double s = Math.Sqrt(p * (p - triangle.A) * (p - triangle.B) * (p - triangle.C));
+                    ViewBag.Area = s;
+                }
+            }
+            
+            return View();
+        }
+    }
+}
---
+++ TriangleApp/Models/Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleApp.Models
+{
+    public class Triangle
+    {
+        [Required(ErrorMessage = "Введите число")]
+        [RegularExpression(@"\d+", ErrorMessage = "Вы ввели некорректные данные")]
+        public double A { get; set; } //катет A
+
+        [Required(ErrorMessage = "Введите число")]
+        [RegularExpression(@"\d+", ErrorMessage = "Вы ввели некорректные данные")]
+        public double B { get; set; } //катет B
+
+        [Required(ErrorMessage = "Введите число")]
+        [RegularExpression(@"\d+", ErrorMessage = "Вы ввели некорректные данные")]
+        public double C { get; set; } //гипотенуза C
+    }
+}
